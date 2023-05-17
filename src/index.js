5df324f84ce0eb93abed5b1225f237308a59b1fb
Victor Alejandro Diaz Jauregui <victor.diaz.jauregui@mi.unc.edu.ar>
2023-05-17 10:49:02 -0300

    Aclaro que los mensajes estan en ASCII

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const messages = [
   
   const funnyCommit = () => {
     const message = messages[Math.floor(Math.random() * messages.length)];
-    console.log(`\x1b[34m${message}\x1b[89m`);
+    console.log(`\x1b[34m Mensaje en ASCII: ${message}\x1b[89m`);
   }
   
   module.exports = {
